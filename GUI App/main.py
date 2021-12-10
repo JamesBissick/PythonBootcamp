1a8fe0aa27533925d816aaf888a1b54bea72c5dd
JamesBissick <bissickj@gmail.com>
2021-12-10 16:38:15 +0100

    :sparkles: - GUI loop exercice

---
+++ GUI App/main.py
@@ -0,0 +1,14 @@
+picture = [
+    [0, 0, 0, 1, 0, 0, 0],
+    [0, 0, 1, 1, 1, 0, 0],
+    [0, 1, 1, 1, 1, 1, 0],
+    [1, 1, 1, 1, 1, 1, 1],
+    [0, 0, 0, 1, 0, 0, 0],
+    [0, 0, 0, 1, 0, 0, 0]
+]
+
+for row in picture:
+    print('\n')
+    for col in row:
+        pixel = ' ' if col == 0 else '*'
+        print(pixel, end='  ')
---
+++ main.py
@@ -0,0 +1,10 @@
+from datetime import date
+
+word = 'abcdef'  # starts at the end
+
+print(word[::])
+print(bool(0))
+
+today = date.today().year
+dob = int(input('What year were you born? '))
+print(f'You are {today - dob} years old.')
