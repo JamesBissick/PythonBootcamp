c2765afca8b821dbda31f6ca5d4c8ec81ecca9c8
James Bissick <bissickj@gmail.com>
2021-12-22 17:57:02 +0100

    ✨ - Basics and fundamentals

---
+++ BasicsI/ex1.py
@@ -0,0 +1,4 @@
+user_name = input('Enter Username: ')
+password = input('Enter Password: ')
+
+print(f'Hello {user_name}, your password {"*" * len(password)} is {len(password)} letters long.')
---
+++ BasicsI/ex2.py
@@ -0,0 +1,19 @@
+is_magician = True
+is_expert = False
+
+# if is_magician and is_expert: 'You are a master magician!'
+# if is_magician and not is_expert: 'At least you're getting there'
+# if not is_magician and is_expert: 'You need magic powers'
+# if not is_magician and not is_expert: 'You need magic powers'
+
+if is_magician and is_expert:
+    print('You are a master magician!')
+
+elif is_magician and not is_expert:
+    print('At least you\'re getting there')
+
+elif not is_magician and is_expert:
+    print('You need magic powers')
+
+else:
+    print('You need magic powers')
